081ee6be0f35a8225c61baf214586b5554bd683c
MooseValley <Moose_Software@yahoo.com.au>
2021-06-11 16:00:51 +1000

    v0.001
    v0.001 - works great

--- JosephusProblem.java
+++ JosephusProblem.java
@@ -1,47 +1,176 @@
-public class JosephusProblem
+/*
+     Author: Moose OMalley, https://github.com/MooseValley/
+       Date: 11-Jun-2021
+My web site: Moose's Sofwtare Valley, Established July-1996, https://moosevalley.github.io/
+     GitHub: https://github.com/MooseValley/Math---Josephus-Problem
+Description: My code to explore the The Josephus Problem, following on from the Numberphile video:
+             https://www.youtube.com/watch?v=uCsD3ZGzMgE
+License:     MIT License - use all or any part of my code, but you must give me full credit.
+
+
+If n = 2^a + L
+where L < 2^a
+then Winner = 2L + 1
+
+eg. If 41 soldiers
+then 41 = 32 + 9
+Winner = 2*9 + 1 = 19
+
+Or In Assembler
+   ROL L
+
+Beautiful !
+
+*/
+import java.util.Scanner;
+
+class Josephus
 {
-   private static boolean[] soldiers;
+   private boolean[] soldiers;
+   private int numSoldiers;
+   Scanner kb = new Scanner (System.in);
 
+   public Josephus ()
+   {
+      this (1);
+   }
 
-   public static int getNextLivingSoldier (int currentSoldier)
+   public Josephus (int numSoldiers)
+   {
+      this.numSoldiers = numSoldiers;
+
+      soldiers = new boolean [numSoldiers];
+   }
+
+   private int getNextLivingSoldier (int currentSoldier)
    {
       int livingSoldierIndex = -1;
 
-      for (int numSoldiers = currentSoldier + 1; numSoldiers < soldiers.length; numSoldiers++)
+      for (int k = 0; k < soldiers.length; k++)
       {
-         if (numSoldiers [numSoldiers] == true) // Living soldier
+         int index = (k + currentSoldier + 1) % soldiers.length;
+
+         //System.out.print ("  -> Looking for LIVE solder at index: " + index);
+
+         if (soldiers [index] == true) // Living soldier
          {
-            livingSoldierIndex = numSoldiers;
-            numSoldiers = soldiers.length; // Exit loop !
+            //System.out.println (" - FOUND !");
+            livingSoldierIndex = index;
+
+            k = soldiers.length; // Exit loop !
          }
       }
 
       return livingSoldierIndex;
    }
 
-   public static int getWinner (int numSoldiers)
+   public int getAliveCount ()
+   {
+      int count = 0;
+
+      for (int index = 0; index < soldiers.length; index++)
+      {
+         if (soldiers [index] == true) // Living soldier
+         {
+            count++;
+         }
+      }
+
+      return count;
+   }
+
+   public int getWinner ()
    {
       boolean done = false;
-      boolean[] soldiers = new boolean [numSoldiers];
 
-      for (int k = 1; k < numSoldiers; k++)
+      for (int k = 0; k < numSoldiers; k++)
       {
          soldiers[k] = true; // Soldier is alive.
       }
 
+      int currSoldier = 0;
 
-      while (done == false)
+      int aliveCount = numSoldiers;
+
+      if (numSoldiers > 1)
       {
-         for (int k = 1; k < numSoldiers; k++)
+         while (aliveCount > 1)
          {
+            int nextLivingSoldier = getNextLivingSoldier (currSoldier);
+
+/*
+            if (nextLivingSoldier < 0)
+            {
+               done = true;
+            }
+            else
+            {
+*/
+               //System.out.println ("-> Killing soldier: " + nextLivingSoldier);
+
+               soldiers[nextLivingSoldier] = false; // He Dead !
+               aliveCount--;
+
+               //System.out.println (toString() );
+
+               currSoldier = getNextLivingSoldier (nextLivingSoldier);
+
+               //kb.nextLine();
+/*
+               if (nextLivingSoldier < 0)
+               {
+                  done = true;
+               }
+               else
+               {
+                  currSoldier = nextLivingSoldier;
+               }
+*/
+            //}
          }
       }
+
+      //System.out.println ("-> Last Living soldier: " + currSoldier);
+
+      return currSoldier + 1; // Convert array index to Count
    }
 
+   public String toString()
+   {
+      StringBuilder sb    = new StringBuilder();
+
+      for (int k = 0; k < soldiers.length; k++)
+      {
+         sb.append ("" + (k+1) ); // Output Solider's starting at 1.
+         if (soldiers [k] == true)
+            sb.append ("(A)");
+          else
+            sb.append ("(D)");
+
+         sb.append (", ");
+      }
+
+      return sb.toString();
+   }
+}
+
+
+public class JosephusProblem
+{
    public static void main (String[] args)
    {
-      for (int numSoldiers = 1; numSoldiers < 20; numSoldiers++)
+      Josephus josephus = new Josephus (5);
+
+      //System.out.println (5 + ": " + josephus.getWinner () );
+
+
+      for (int numSoldiers = 1; numSoldiers <= 20; numSoldiers++)
       {
+         josephus = new Josephus (numSoldiers);
+
+         System.out.println (numSoldiers + ": " + josephus.getWinner() +
+                             "  -> " + josephus.toString() );
       }
+
    }
 }
